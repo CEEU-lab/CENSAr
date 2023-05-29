3c4cbe3d5c743f39c77596e79f1d04753d6e3a88
PyMap <44501803+PyMap@users.noreply.github.com>
2023-05-29 18:01:33 -0300

    geoprocessing docstring

--- CENSAr/vector_data_analysis/geoprocessing.py
+++ CENSAr/vector_data_analysis/geoprocessing.py
@@ -1,28 +1,61 @@
-import pandas as pd
 import geopandas as gpd
 from shapely import wkt
 
 def from_wkt(df, wkt_column, crs=4326):
-    '''
-    Crea un geodataframe a partir de una columna de geometria de tipo object/string
-    '''
+    """
+    Loads a shapely geometry from well known text column.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe with wkt geometry representation.
+    wkt_column : str
+        Name of the wkt column.
+    crs : str | int, default 4326
+        Coordinate reference system.
     
+    Returns
+    -------
+    gdf:gpd.GeoDataFrame
+        Table with shapely geometry representation.
+    """
     df["geometry"]= df[wkt_column].apply(wkt.loads) 
     gdf = gpd.GeoDataFrame(df, geometry='geometry',crs=crs) 
     
     return gdf
 
 def from_coarser_to_thiner_area(coarser_geom, thiner_geom,coarser_idx, thiner_idx):
+    """
+    Returns the total number of households or residential units for a given
+    category with his distribution by census tract.
 
-     thiner_geom['thiner_area'] = thiner_geom.geometry.area
-     coarser_geom['coarser_area'] = coarser_geom.geometry.area
+    Parameters
+    ----------
+    coarser_geom : gpd.GeoDataFrame
+        Geodataframe with coarser area Polygon geometries.
+    thiner_geom : gpd.GeoDataFrame
+        Geodataframe with thiner area Polygon geometries contained in coarser ones.
+    coarser_idx : str 
+        Name of the index column to identify each Polygon geometry (e.g.:"link"))
+    thiner_idx : str 
+        Name of the index column to identify each Polygon geometry (e.g.:"renabap_id"))
 
-     ovl = gpd.overlay(coarser_geom, thiner_geom, how = "intersection", keep_geom_type=False)
-     ovl['ovl_area'] = ovl.geometry.area
+    Returns
+    -------
+    proportions:pd.DataFrame
+        Table indicating the area overlay for thiner and coarser geometries and their respective shares.
+    """
 
-     proportions = ovl[[thiner_idx, coarser_idx,'thiner_area', 'coarser_area', 'ovl_area']].copy()
-     proportions['thiner_share'] = proportions['ovl_area']/proportions['thiner_area']*100
-     proportions['thiner_share'] = proportions['thiner_share'].round(1)
-     proportions['coarser_share'] = proportions['ovl_area']/proportions['coarser_area']*100
-     proportions['coarser_share'] = proportions['coarser_share'].round(1)
-     return proportions
+    thiner_geom['thiner_area'] = thiner_geom.geometry.area
+    coarser_geom['coarser_area'] = coarser_geom.geometry.area
+
+    ovl = gpd.overlay(coarser_geom, thiner_geom, how = "intersection", keep_geom_type=False)
+    ovl['ovl_area'] = ovl.geometry.area
+
+    proportions = ovl[[thiner_idx, coarser_idx,'thiner_area', 'coarser_area', 'ovl_area']].copy()
+    proportions['thiner_share'] = proportions['ovl_area']/proportions['thiner_area']*100
+    proportions['thiner_share'] = proportions['thiner_share'].round(1)
+    proportions['coarser_share'] = proportions['ovl_area']/proportions['coarser_area']*100
+    proportions['coarser_share'] = proportions['coarser_share'].round(1)
+    
+    return proportions
